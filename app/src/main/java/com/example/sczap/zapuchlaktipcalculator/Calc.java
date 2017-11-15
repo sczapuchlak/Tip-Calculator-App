ad177f024e73e458b02440554ea4d8a970a912b0
stephanie zap <iamstephanie@gmail.com>
2017-11-15 13:40:20 -0600

    App is working... Just need to fix strings

--- app/src/main/java/com/example/sczap/zapuchlaktipcalculator/Calc.java
+++ app/src/main/java/com/example/sczap/zapuchlaktipcalculator/Calc.java
@@ -7,37 +7,65 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.EditText;
 
-import org.w3c.dom.Text;
 
 public class Calc extends AppCompatActivity {
-    EditText personUserEntered;
-    EditText tipPercentUserEntered;
-    EditText billTotalUserEntered;
-    Button calculateButton;
-    Button resetButton;
-    TextView tipAmountCalc;
-    TextView totalWithtipCalc;
-    TextView eachPersonCalc;
 
+    //find all of the inputs we will need to reference
+    EditText tipPercentUserEntered = (EditText) findViewById(R.id.tipPercentUserInput);
+    EditText billTotalUserEntered = (EditText) findViewById(R.id.billTotalUserInput);
+    TextView tipAmountCalc = (TextView) findViewById(R.id.tipAmountCalc);
+    TextView totalWithtipCalc = (TextView) findViewById(R.id.totalWithTipCalc);
+    TextView eachPersonCalc = (TextView) findViewById(R.id.eachUserPaysCalc);
+    EditText personUserEntered = (EditText)  findViewById(R.id.personUserInput);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calc);
-        personUserEntered =  findViewById(R.id.personUserInput);
-        personUserEntered =  findViewById(R.id.tipPercentUserInput);
-        personUserEntered =  findViewById(R.id.billTotalUserInput);
-        calculateButton =  findViewById(R.id.calculateButton);
 
-        calculateButton.setOnClickListener(new View.OnClickListener(){
-           public void onClick(View view){
-               tipAmountCalc = findViewById(R.id.tipAmountCalc);
-               totalWithtipCalc = findViewById(R.id.totalWithTipCalc);
-               eachPersonCalc = findViewById(R.id.eachUserPaysCalc);
+        //attach the listener to the calculate button
+        Button calcButton = (Button) findViewById(R.id.calculateButton);
+        calcButton.setOnClickListener(new CalculateClickListener());
 
-               tipAmountCalc.setText("Just seeing if this works");
-           }
-        });
+        //attach the listener to the reset button
+        Button resetButton = (Button) findViewById(R.id.resetButton);
+        resetButton.setOnClickListener(new ResetClickListener());
+
     }
 
+    private class CalculateClickListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View view){
+
+                //calculate tip amount from the percentage entered and the total bill amount
+                //and then set the text in the calc slot
+                int billAmount = Integer.parseInt(billTotalUserEntered.getText().toString());
+                int getTip = Integer.parseInt(tipPercentUserEntered.getText().toString());
+                int tipAmount = getTip*billAmount/100;
+                tipAmountCalc.setText(Integer.toString(tipAmount));
+
+                //calculate total with the tip , and then set the text in the calc slot
+                int totalPlusTip= billAmount+tipAmount;
+                totalWithtipCalc.setText(Integer.toString(totalPlusTip));
+
+                //calculate amount per person , and then set the text in the calc slot
+                int personAmount = Integer.parseInt(personUserEntered.getText().toString());
+                int eachPersonOwes= totalPlusTip/personAmount;
+                eachPersonCalc.setText(Integer.toString(eachPersonOwes));
+           }
+    }
+
+     public class ResetClickListener implements View.OnClickListener {
+         @Override
+         public void onClick(View view) {
+             tipPercentUserEntered.setText(String.format(""));
+             billTotalUserEntered.setText(String.format(""));
+             tipAmountCalc.setText(String.format(""));
+             totalWithtipCalc.setText(String.format(""));
+             eachPersonCalc.setText(String.format(""));
+             personUserEntered.setText(String.format(""));
+
+         }
+         }
 }
